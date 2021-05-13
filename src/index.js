f69d7b5d370adc7dc010e52133332713a7be5ba9
Alexander Melnik <alexander.melnik@pandadoc.com>
2021-05-13 11:18:24 +0300

    added first request

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+	type Query {
+		info: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		info: () => 'This is the API of a HackerNews Clone'
+	}
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
